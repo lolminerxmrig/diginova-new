03345a6feaf094104cb65a34967b798d1f0c4954
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2020-12-28 18:22:30 +0330

    fix(seeder): register auth seeder for staff and customer module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call('Modules\Staff\Auth\Database\seeders\StaffSeeder');
+        $this->call('Modules\Customers\Auth\Database\seeders\CustomerSeeder');
     }
 }
