47c6650b9b4e4d4a0a9b880039534987b2794d47
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2021-01-03 11:32:48 +0330

    feat(staff/category): add seeders to staff category

---
+++ Modules/Staff/Category/Database/seeders/CategorySeeder.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Modules\Staff\Category\Database\seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Staff\Category\Models\Category;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name' => 'کالای دیچیتال',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 0,
+        ]);
+
+        Category::create([
+            'name' => 'مذ و پوشاک',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 0,
+        ]);
+
+        Category::create([
+            'name' => 'لوازم جانبی گوشی',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 1,
+        ]);
+
+        Category::create([
+            'name' => 'گوشی موبایل',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 2,
+        ]);
+
+        Category::create([
+            'name' => 'لباس مردانه',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 3,
+        ]);
+
+        Category::create([
+            'name' => 'لباس زنانه',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 3,
+        ]);
+
+        Category::create([
+            'name' => 'سامسونگ',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 4,
+        ]);
+
+        Category::create([
+            'name' => 'هواوی',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 2,
+        ]);
+
+        Category::create([
+            'name' => 'ساده',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 4,
+        ]);
+
+        Category::create([
+            'name' => 'لمسی',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 4,
+        ]);
+
+        Category::create([
+            'name' => 'سری a',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 6,
+        ]);
+
+        Category::create([
+            'name' => 'سری b',
+            'en_name' => 'digital',
+            'image_id' => 1,
+            'slug' => 'test',
+            'parent_id' => 1,
+        ]);
+    }
+}
