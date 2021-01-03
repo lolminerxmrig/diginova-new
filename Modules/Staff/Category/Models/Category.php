1d280ccee2e6870fe076cf53fd047fa40748e680
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2021-01-03 11:31:23 +0330

    feat(category/models): add models to staff/category module

---
+++ Modules/Staff/Category/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Staff\Category\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'en_name', 'image_id', 'parent_id', 'slug'];
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id', 'id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+
+    }
+}
+
---
+++ Modules/Staff/Category/Models/Media.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Staff\Category\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Media extends Model
+{
+    use HasFactory;
+
+    protected $table = 'media';
+    protected $fillable = ['name', 'path', 'person_role', 'person_id'];
+}
+
