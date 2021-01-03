f23b92cfbb23c6f833a1ba986f0117922eec9c9f
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2021-01-03 11:06:45 +0330

    fix(route): add route name for / page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('indexPage');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
