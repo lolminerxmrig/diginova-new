18626104d1a0501727ebaa4551e569f872803502
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2021-04-18 11:23:34 +0430

    temp

--- Modules/Staff/Slider/Database/slider_images.sql
+++ Modules/Staff/Slider/Database/slider_images.sql
@@ -1,44 +1,37 @@
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-A (بنر)', 'active', '2800x100', 1, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-B (بنر)', 'active', '1645x105', 1, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-C (بنر)', 'active', '856x428', 1, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-D (بنر)', 'active', '856x428', 1, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-E (اسلایدر)', 'active', '1780x890', 1, null, null);
 --
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-1 (گروه)', 'active', '400x300', 2, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-2 (گروه)', 'active', '820X300', 2, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-3 (گروه)', 'active', '400x300', 2, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-4 (گروه)', 'active', '820X300', 2, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-5 (گروه)', 'active', '1645x208', 2, null, null);
+-- Dumping data for table `slider_images`
 --
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MH-A (اسلایدر)', 'active', '1837x329', 3, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-3 (گروه)', 'active', '820x300', 3, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-2 (گروه)', 'active', '820x300', 3, null, null);
--- INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-1 (گروه)', 'active', '1780x890', 3, null, null);
-INSERT INTO `slider_images` (`id`, `alt`, `link`, `status`, `slider_id`, `created_at`, `updated_at`) VALUES
-(1, NULL, NULL, 'active', 5, NULL, NULL),
-(2, NULL, NULL, 'active', 5, NULL, NULL),
-(3, NULL, NULL, 'active', 5, NULL, NULL),
-(6, NULL, NULL, 'active', 12, NULL, NULL),
-(7, NULL, NULL, 'active', 12, NULL, NULL),
-(8, NULL, NULL, 'active', 12, NULL, NULL),
-(9, NULL, NULL, 'active', 12, NULL, NULL),
-(10, NULL, NULL, 'active', 14, NULL, NULL),
-(11, NULL, NULL, 'active', 14, NULL, NULL),
-(12, NULL, NULL, 'active', 14, NULL, NULL),
-(13, NULL, NULL, 'active', 14, NULL, NULL),
-(14, NULL, NULL, 'active', 6, NULL, NULL),
-(15, NULL, NULL, 'active', 6, NULL, NULL),
-(16, NULL, NULL, 'active', 6, NULL, NULL),
-(17, NULL, NULL, 'active', 6, NULL, NULL),
-(18, NULL, NULL, 'active', 7, NULL, NULL),
-(19, NULL, NULL, 'active', 7, NULL, NULL),
-(20, NULL, NULL, 'active', 8, NULL, NULL),
-(21, NULL, NULL, 'active', 8, NULL, NULL),
-(22, NULL, NULL, 'active', 8, NULL, NULL),
-(23, NULL, NULL, 'active', 8, NULL, NULL),
-(24, NULL, NULL, 'active', 9, NULL, NULL),
-(25, NULL, NULL, 'active', 9, NULL, NULL),
-(26, NULL, NULL, 'active', 13, NULL, NULL),
-(27, NULL, NULL, 'active', 13, NULL, NULL),
-(28, NULL, NULL, 'active', 11, NULL, NULL),
-(29, NULL, NULL, 'active', 11, NULL, NULL);
+
+INSERT INTO `slider_images` (`id`, `alt`, `link`, `status`, `slider_id`, `position`, `created_at`, `updated_at`) VALUES
+(1, 'a-pic1', 'pic1', 'active', 5, 0, NULL, '2021-04-17 11:45:32'),
+(2, 'a-pic-2', 'pic2', 'active', 5, 1, NULL, '2021-04-17 11:45:32'),
+(3, 'a-pic3', 'pic3', 'active', 5, 2, NULL, '2021-04-17 11:45:32'),
+(6, NULL, NULL, 'active', 12, NULL, NULL, NULL),
+(7, NULL, NULL, 'active', 12, NULL, NULL, NULL),
+(8, NULL, NULL, 'active', 12, NULL, NULL, NULL),
+(9, NULL, NULL, 'active', 12, NULL, NULL, NULL),
+(10, NULL, NULL, 'active', 14, NULL, NULL, NULL),
+(11, NULL, NULL, 'active', 14, NULL, NULL, NULL),
+(12, NULL, NULL, 'active', 14, NULL, NULL, NULL),
+(13, NULL, NULL, 'active', 14, NULL, NULL, NULL),
+(14, 'alt-g1', 'g1', 'active', 6, 0, NULL, '2021-04-17 15:20:44'),
+(15, 'alt-g2', 'g2', 'active', 6, 1, NULL, '2021-04-17 15:20:44'),
+(16, 'alt-g3', 'g3', 'active', 6, 2, NULL, '2021-04-17 15:20:44'),
+(17, 'alt-g4', 'g4', 'active', 6, 3, NULL, '2021-04-17 15:20:45'),
+(18, 'alt-1', '#', 'active', 7, 0, NULL, '2021-04-17 16:05:27'),
+(19, 'alt-2', '#', 'active', 7, 1, NULL, '2021-04-17 16:05:27'),
+(20, 'h1-alt', '/h1', 'active', 8, 0, NULL, '2021-04-17 17:03:03'),
+(21, 'h2-alt', '/h2', 'active', 8, 1, NULL, '2021-04-17 17:03:03'),
+(22, 'h3-alt', '/h3', 'active', 8, 2, NULL, '2021-04-17 17:03:03'),
+(23, 'h4-alt', '/h4', 'active', 8, 3, NULL, '2021-04-17 17:03:03'),
+(24, 't1', 'test1', 'active', 9, 0, NULL, '2021-04-17 19:07:58'),
+(25, 't2', 'test2', 'active', 9, 1, NULL, '2021-04-17 19:07:58'),
+(26, NULL, NULL, 'active', 13, NULL, NULL, NULL),
+(27, NULL, NULL, 'active', 13, NULL, NULL, NULL),
+(28, NULL, NULL, 'active', 11, NULL, NULL, NULL),
+(29, NULL, NULL, 'active', 11, NULL, NULL, NULL),
+(30, 'بنر 1', 'banner-1', NULL, 1, NULL, '2021-04-17 10:24:54', '2021-04-17 13:06:15'),
+(31, 'بنر 2', 'banner-2', NULL, 2, NULL, '2021-04-17 10:24:54', '2021-04-17 13:06:16'),
+(32, 'بنر چپ 2', '/left-banner-2', NULL, 3, NULL, '2021-04-17 10:24:54', '2021-04-17 10:24:54'),
+(33, 'بنر چپ 1', '/left-banner-1', NULL, 4, NULL, '2021-04-17 10:24:55', '2021-04-17 10:24:55'),
+(34, 'tt', 'تست تست', NULL, 10, NULL, '2021-04-17 19:18:59', '2021-04-17 19:19:33');
--- Modules/Staff/Slider/Database/sliders.sql
+++ Modules/Staff/Slider/Database/sliders.sql
@@ -1,16 +1,19 @@
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-A (بنر)', 'active', '2800x100', 1, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-B (بنر)', 'active', '1645x105', 1, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-C (بنر)', 'active', '856x428', 1, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-D (بنر)', 'active', '856x428', 1, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HH-E (اسلایدر)', 'active', '1780x890', 1, null, null);
+--
+-- Dumping data for table `sliders`
+--
 
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-1 (گروه)', 'active', '400x300', 2, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-2 (گروه)', 'active', '820X300', 2, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-3 (گروه)', 'active', '400x300', 2, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-4 (گروه)', 'active', '820X300', 2, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('HB-5 (گروه)', 'active', '1645x208', 2, null, null);
-
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MH-A (اسلایدر)', 'active', '1837x329', 3, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-3 (گروه)', 'active', '820x300', 3, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-2 (گروه)', 'active', '820x300', 3, null, null);
-INSERT INTO diginova.sliders (name, status, size, group_id, created_at, updated_at) VALUES ('MB-1 (گروه)', 'active', '1780x890', 3, null, null);
+INSERT INTO `sliders` (`id`, `name`, `en_name`, `status`, `size`, `type`, `group_id`, `created_at`, `updated_at`) VALUES
+(1, 'A (بنر)', 'A', 'active', '2800x100', 'banner', 1, NULL, NULL),
+(2, 'B (بنر)', 'B', 'active', '1645x105', 'banner', 1, NULL, NULL),
+(3, 'C (بنر)', 'C', 'active', '856x428', 'banner-r', 1, NULL, NULL),
+(4, 'D (بنر)', 'D', 'active', '856x428', 'banner-r', 1, NULL, NULL),
+(5, 'E (اسلایدر)', 'E', 'active', '1780x890', 'slider-r', 1, NULL, NULL),
+(6, 'F (گروه)', 'F', 'active', '400x300', 'banner-group', 2, NULL, NULL),
+(7, 'G (گروه)', 'G', 'active', '820X300', 'banner-group', 2, NULL, NULL),
+(8, 'H (گروه)', 'H', 'active', '400x300', 'banner-group', 2, NULL, NULL),
+(9, 'I (گروه)', 'I', 'active', '820X300', 'banner-group', 2, NULL, NULL),
+(10, 'J (بنر)', 'J', 'active', '1645x208', 'banner', 2, NULL, NULL),
+(11, 'K (اسلایدر)', 'K', 'active', '1837x329', 'slider', 3, NULL, NULL),
+(12, 'L (گروه)', 'L', 'active', '820x300', 'banner-group', 3, NULL, NULL),
+(13, 'M (گروه)', 'M', 'active', '820x300', 'banner-group', 3, NULL, NULL),
+(14, 'N (گروه)', 'N', 'active', '1780x890', 'banner-group', 3, NULL, NULL);
