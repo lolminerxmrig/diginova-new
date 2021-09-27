4156ce2b652ce2383a43d58085251804cdafe74f
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2021-09-27 14:40:59 +0330

    temp

---
+++ Modules/Staff/ProductSwiper/Models/ProductSwiper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Staff\ProductSwiper\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Staff\Category\Models\Category;
+
+
+class ProductSwiper extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'category_id', 
+        'sort_by', 
+        'status', 
+        'position'
+    ];
+
+    /**
+     * Get the category that owns the ProductSwiper
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
+
